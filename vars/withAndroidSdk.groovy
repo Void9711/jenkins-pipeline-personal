a906d8f8d2cfa43aaea2006a3920fddfbf825e41
陶爽 <taoshuang@kingsoft.com>
2020-05-14 16:35:22 +0800

    AndroidSdk -> android-sdk.

--- vars/withAndroidSdk.groovy
+++ vars/withAndroidSdk.groovy
@@ -3,6 +3,6 @@
 // AndroidSdk is configured via Custom Tools Plugin: https://plugins.jenkins.io/custom-tools-plugin
 // Manage Jenkins -> Global Tool Configuration
 def call(String version, Closure body) {
-    def androidSdkHome = tool name: "AndroidSdk-${version}", type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+    def androidSdkHome = tool name: "android-sdk-${version}", type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
     withEnv(["ANDROID_HOME=${androidSdkHome}"], body)
 }
