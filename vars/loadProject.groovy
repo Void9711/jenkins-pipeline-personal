082e3af6159ded5c5897a4f48d6a60d863dbbf1b
陶爽 <taoshuang@kingsoft.com>
2020-06-19 16:57:53 +0800

    Add library loadProject.

---
+++ vars/loadProject.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+def call(String id, boolean debug = false) {
+    def resource = libraryResource 'projects.yml'
+    def settings = readYaml(text: resource)
+    def projects = settings.projects
+
+    projects.each {
+        project -> 
+        if (project.id == id) {
+            if (debug) {
+                printProject(project)
+            }
+            return project
+        }
+    }
+    echo "Requested project ${id} not found."
+    return ''
+}
+
+def printProject(project) {
+    def json = JsonOutput.toJson(project)
+    json = JsonOutput.prettyPrint(json)
+    echo "[loadProject] ${project.id} - ${project.name}\n${json}"
+}
